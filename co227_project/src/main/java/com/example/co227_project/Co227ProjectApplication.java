04db98bd4955037382f965c72df360bb3a0cc720
unknown <e18149@eng.pdn.ac.lk>
2022-06-07 15:35:52 +0530

    Added a spring boot project and the zip folder

---
+++ co227_project/src/main/java/com/example/co227_project/Co227ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.co227_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Co227ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Co227ProjectApplication.class, args);
+	}
+
+}
---
+++ co227_project/src/test/java/com/example/co227_project/Co227ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.co227_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Co227ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
