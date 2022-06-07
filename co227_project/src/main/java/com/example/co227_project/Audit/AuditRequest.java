fb9f914c7c0a249cc0dfd9d693158111ea92fade
unknown <e18149@eng.pdn.ac.lk>
2022-06-07 16:06:17 +0530

    classes AuditController , AuditRequest and interfave AuditRepository created

---
+++ co227_project/src/main/java/com/example/co227_project/Audit/AuditRequest.java
@@ -0,0 +1,121 @@
+package com.example.co227_project.Audit;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class AuditRequest {
+
+    @JsonProperty("ClientTime")
+    private String clientTime;
+
+    @JsonProperty("ServerTime")
+    private String server_time;
+
+    @JsonProperty("Ip")
+    private String ip;
+
+    @JsonProperty("Type")
+    private String type;
+
+    @JsonProperty("Serverity")
+    private String serverity;
+
+    @JsonProperty("Keyword")
+    private String keyword;
+
+    @JsonProperty("Description")
+    private String description;
+
+    @JsonProperty("GeneratedFrom")
+    private String generated_from;
+
+    @JsonProperty("Remarks")
+    private String remarks;
+
+    public AuditRequest() {
+    }
+
+    public AuditRequest(String clientTime, String server_time, String ip, String type, String serverity, String keyword, String description, String generated_from, String remarks) {
+        this.clientTime = clientTime;
+        this.server_time = server_time;
+        this.ip = ip;
+        this.type = type;
+        this.serverity = serverity;
+        this.keyword = keyword;
+        this.description = description;
+        this.generated_from = generated_from;
+        this.remarks = remarks;
+    }
+
+    public String getClientTime() {
+        return clientTime;
+    }
+
+    public void setClientTime(String clientTime) {
+        this.clientTime = clientTime;
+    }
+
+    public String getServer_time() {
+        return server_time;
+    }
+
+    public void setServer_time(String server_time) {
+        this.server_time = server_time;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getServerity() {
+        return serverity;
+    }
+
+    public void setServerity(String serverity) {
+        this.serverity = serverity;
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getGenerated_from() {
+        return generated_from;
+    }
+
+    public void setGenerated_from(String generated_from) {
+        this.generated_from = generated_from;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+}
