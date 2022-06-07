05b33ecf4ed36dcc71347ba95db506dd9bba79f4
unknown <e18149@eng.pdn.ac.lk>
2022-06-07 16:10:49 +0530

    User package and the class created

---
+++ co227_project/src/main/java/com/example/co227_project/User/User.java
@@ -0,0 +1,48 @@
+package com.example.co227_project.User;
+
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("users")
+public class User {
+
+    @Id
+    private String id;
+
+    private String Username;
+
+    private String Password;
+
+    public User() {
+    }
+
+    public User(String username, String password) {
+        Username = username;
+        Password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+}
